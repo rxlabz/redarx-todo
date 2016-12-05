7dc3270dd3667703a6b10e0b23cb73576375722e
rxlabz <dev@rxlabz.com>
2016-12-05 13:43:04 +0100

    Refactor : ui-helper a({href, label, onClick}) achor shortcut

--- lib/components/todo-footer.dart
+++ lib/components/todo-footer.dart
@@ -3,20 +3,22 @@ import 'dart:html';
 import 'package:redarx/redarx.dart';
 import 'package:todo_redarx/commands.dart';
 import 'package:todo_redarx/components/component-base.dart';
+import 'package:todo_redarx/components/ui-helper.dart';
 
 class TodoFooter extends ComponentBase {
   SpanElement label;
   AnchorElement btClear;
   AnchorElement btToggle;
 
-  set showCompleted(bool value){
-
-      btToggle.text = value ? "Remaining ( $_numRemaining )" : "Completed ( $_numCompleted )";
-      label.text = value ? "Completed : ${_numCompleted}" : "Remaining : ${_numRemaining}";
+  set showCompleted(bool value) {
+    btToggle.text =
+        value ? "Remaining ( $_numRemaining )" : "Completed ( $_numCompleted )";
+    label.text =
+        value ? "Completed : ${_numCompleted}" : "Remaining : ${_numRemaining}";
   }
 
   int _numCompleted;
-  set numCompleted(int value) =>_numCompleted = value;
+  set numCompleted(int value) => _numCompleted = value;
 
   int _numRemaining;
   set numRemaining(int value) => _numRemaining = value;
@@ -25,15 +27,16 @@ class TodoFooter extends ComponentBase {
     label = new SpanElement()..text = "Archives :";
     addChild(label);
 
-    btClear = new AnchorElement(href: '#')
-      ..text = "Clear"
-      ..onClick
-          .listen((e) => dispatch(new Request(RequestType.CLEAR_ARCHIVES)));
+    btClear = a(
+        href: '#',
+        label: "Clear",
+        onClick: (e) => dispatch(new Request(RequestType.CLEAR_ARCHIVES)));
 
-    btToggle = new AnchorElement(href: '#')
-      ..text = "Show completed"
-      ..onClick.listen(
-          (e) => dispatch(new Request(RequestType.TOGGLE_SHOW_COMPLETED)));
+    btToggle = a(
+        href: '#',
+        label: "Show completed",
+        onClick: (e) =>
+            dispatch(new Request(RequestType.TOGGLE_SHOW_COMPLETED)));
     addChildren([label, btToggle, btClear]);
   }
 
--- lib/components/ui-helper.dart
+++ lib/components/ui-helper.dart
@@ -1,6 +1,13 @@
 import 'dart:html';
 
-DivElement div({String classes, String id}) => new DivElement()..classes.addAll(csspl(classes))..id = id;
+DivElement div({String classes, String id}) => new DivElement()
+  ..classes.addAll(csspl(classes))
+  ..id = id;
+
+AnchorElement a({href: "#", String label, onClick}) =>
+    new AnchorElement(href: href)
+      ..text = label
+      ..onClick.listen((e) => onClick(e));
 
 /**
  * split a css multi-class string tl Iterable
