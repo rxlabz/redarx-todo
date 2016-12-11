91b8278b05ec3ff4dafea80c6294dfcd26722e7a
rxlabz <dev@rxlabz.com>
2016-12-11 23:05:53 +0100

    Feat : Todo fromMap constructor

--- lib/model/todo.dart
+++ lib/model/todo.dart
@@ -7,6 +7,12 @@ class Todo {
     if (uid == null) uid = new DateTime.now().millisecondsSinceEpoch;
   }
 
+  Todo.fromMap(Map<String, dynamic> data){
+    this.uid = data['uid'];
+    this.label = data['label'];
+    this.completed = data['completed'] == 1;
+  }
+
   @override
   String toString() {
     return 'Todo{ $uid , $label }';
